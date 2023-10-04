895f83793c3b5a40c894fed08340b4670bda45d0
Ikmal Hakim <ikmal.hakim.mohd.borhan@my.ey.com>
2023-10-04 17:52:42 +0800

    remove stuff on app.config

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   ui: {
     notifications: {
       // Show toasts at the top right of the screen
-      wrapper: 'fixed flex flex-col justify-end z-[55]',
-      position: 'top-0 right-0'
+      // wrapper: 'fixed flex flex-col justify-end z-[55]',
+      // position: 'top-0 right-0'
     }
   }
 })
