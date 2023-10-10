26f4d89d217d2085ecdd7575b20bdaa18f48c657
Ikmal Hakim <ikmal.hakim.mohd.borhan@my.ey.com>
2023-10-10 17:00:52 +0800

    try debug form

--- pages/form.vue
+++ pages/form.vue
@@ -1,6 +1,8 @@
 <template>
    <form name="contact2" action="/form" data-netlify="true" data-netlify-honeypot="bot-field" method="POST">
+    <input type="hidden" name="form-name" value="contact2" />
     <label for="name">Name</label>
-    <input name="first_name" type="text">
+    <input name="first_name" id="first_na" type="text">
+    <button type="submit">submit</button>
    </form>
 </template>
